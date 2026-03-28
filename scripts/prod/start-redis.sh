b59b65cddf6732405c993f07288d21b838ebb17a
Catflare <nekoflare@users.noreply.github.com>
2026-03-28 12:39:47 +0100

    no network

--- scripts/prod/start-redis.sh
+++ scripts/prod/start-redis.sh
@@ -12,7 +12,6 @@ echo "Starting chat-redis..."
 docker run -d \
   --name chat-redis \
   --restart unless-stopped \
-  --network "${PROJECT}_app_network" \
   --memory 256m --cpus 0.2 \
   -p 127.0.0.1:6379:6379 \
   -v chat-redis_data:/data \
